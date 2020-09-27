e0724c6b0d8bedbab9812475f14867d872e11861
Rafael Thiago <60414261+RafaelTLS@users.noreply.github.com>
2020-09-26 21:42:40 -0400

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,17 @@
 	<title>Masturbyte</title>
 	<link rel="icon" href="https://i.imgur.com/MWavXXN.png">
 	<link rel="stylesheet" type="text/css" href="css.css">
+	<meta property="og:image" name="https://i.imgur.com/aaULAVU.png">
+	<meta property="og:title" name="Masturbyte">
+	<meta property="og:type" name="game">
+	<meta property="og:url" name="https://rafaeltls.github.io/jooj">
+	<meta property="og:locale" content="pt_BR">
+	<meta name="description" content="O site de games mais pica da galáxia!">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="Masturbyte">
+	<meta name="twitter:description" content="O site de games mais pica da galáxia!">
+	<meta name="twitter:image" content="https://i.imgur.com/aaULAVU.png">
+	<meta name="theme-color" content="#eb4034">
 </head>
 <body>
 	<div class="header">
